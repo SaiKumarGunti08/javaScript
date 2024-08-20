2f745da1972cfd21e2c6ab9ffb4b8635bd86ade2
SaiKumarGunti08 <167128074+SaiKumarGunti08@users.noreply.github.com>
2024-08-20 12:43:26 +0000

    DataTypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,14 @@
+"use strict"// treats all js code as newer version
+// alert(3+3 or "name" or etc); // it is not used in node.js , it is used in browser (is shows alart slides nd result in it)
+
+console.log("sai");
+
+let Name="sai" //string
+let age=24     //number or integer     
+let state=true //or false // boolean
+console.log(typeof Name)//string op
+console.log(typeof age) // integer
+console.log(typeof state)//boolean
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined   
+

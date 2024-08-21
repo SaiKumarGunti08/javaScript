59f9c5d657761093713af427640c3f3b2f94e3b8
SaiKumarGunti08 <167128074+SaiKumarGunti08@users.noreply.github.com>
2024-08-21 08:51:53 +0000

    coversions

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,27 @@
+//----comparsion-----
+// console.log(2==1);
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<=1);
+
+//cannot expect a perticular value
+
+// console.log(null==1);
+// console.log(null>0);
+// console.log(null>=0);
+
+// console.log(undefined==0);
+// console.log(undefined>0);
+// console.log(undefined<0);
+
+// below values are converted to integer("2"=2) 
+
+// console.log("2"==1);
+// console.log("2">1);
+// console.log("2"<1);
+// console.log("2">=1);
+
+//===  (strict check) it checks the valuse nd type also 
+
+console.log("2"===1);
+console.log("2"===2); // both are 2 but datatype is different

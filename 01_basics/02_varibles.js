d67c34ba17419c7b5d2e7b3837918319b4f4ad18
SaiKumarGunti08 <167128074+SaiKumarGunti08@users.noreply.github.com>
2024-08-21 07:29:12 +0000

    conversion

---
+++ 01_basics/02_varibles.js
@@ -0,0 +1,24 @@
+const accountId=1233
+let accountEmail="sai@gmail.com"
+var accPass="123Sai"
+accountCity="hyd"   // a variable cound be declared but not a best prectice
+let accountName;
+ // undefined
+
+// accountId=2 //Not allowed cannot be changed when its const
+//below can be changed  
+// accountEmail="chintu@.com"
+// accPass="1111"
+// accountCity="DElhi"
+
+/*
+can write any thing in between this comments in how many lines u want
+
+Prefere not to use Var 
+because of isseue in blocking scope and functional scope
+*/
+
+console.table([accountId,accountEmail,accPass,accountCity, accountName])
+
+
+
---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,26 @@
+//Type conversion
+
+//using Number(typeof score)
+//string()
+
+//let score="33"  //op=> number
+let score=33    //op=> number
+// let score="33abc" //. op=>NaN
+//let score=true //or false  op=>o or 1
+// let score=null    //   op=>0
+// let score=undefined  //op=>NaN
+// let value=Number(score) //converts to number
+// let ctoS=String(score) //converts to string
+// let ctoB=Boolean(score)
+// console.log(ctoS)
+// console.log(typeof ctoS);
+// console.log(typeof value);
+// console.log(score);
+// console.log(typeof score);
+
+
+//let value="" //empty string=> false, "hshshsh"=>true
+let value=1 // 1=>true ,0=>false
+let cofvalue=Boolean(value)
+console.log(cofvalue)
+console.log(typeof cofvalue);

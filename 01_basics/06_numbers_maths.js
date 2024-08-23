4993f1b04410c3ee46c5a5c0c37b60ec078b7b30
SaiKumarGunti08 <167128074+SaiKumarGunti08@users.noreply.github.com>
2024-08-23 13:59:30 +0000

    Number And Maths

---
+++ 01_basics/06_numbers_maths.js
@@ -0,0 +1,40 @@
+// const frstNum=400
+// const secNum=new Number(198.8930)
+
+// console.log(frstNum);
+// console.log(frstNum.toString().length); // for converting to string =>toString()
+// console.log(frstNum.toFixed(2));
+// console.log(secNum.toPrecision(4));
+// console.log();
+
+// //for seening as amounts
+// const Amount=100000000000
+// console.log(Amount.toLocaleString("en-IN"));
+
+//++++++++++++++++Maths+++++++++++++++
+
+// const mthArry=[1,2,-3,-4,-5]
+// console.log(Math);
+// console.log(Math.abs()); // abs() is used to convert the negative values to positive.
+// console.log(Math.round(4.5)); // it takes round figure =>if greater than 0.5 then next value 
+// console.log(Math.ceil(4.05));//if even some value is more then actual value then take next value =>4.0005 as 0005 is more than 4.0  op=5
+// console.log(Math.floor(4.9));//it takes lower value
+// console.log(Math.sqrt(25));// square root values
+// console.log(Math.min(1,2,3,4,5,6,7,8,9));//min value
+// console.log(Math.max(1,2,3,4,5,6,7,8,9));//max value
+console.log(Math.random());// takes a random num between 0 nd 1
+//to get Random num above 0 eg console.log((Math.Random()*10)+1)
+console.log((Math.random()*10)+1)
+//______to get Between the Values__________
+const MaxVal=20
+const MinVal=10
+console.log(Math.floor(Math.random()*(MaxVal-MinVal+1)+MinVal))
+
+
+
+
+
+
+
+
+

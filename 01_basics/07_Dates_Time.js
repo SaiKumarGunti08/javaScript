9f8e30bab6402ed9b883aab203d05bf5f2b8c325
SaiKumarGunti08 <167128074+SaiKumarGunti08@users.noreply.github.com>
2024-08-23 14:45:59 +0000

    date

---
+++ 01_basics/07_Dates_Time.js
@@ -0,0 +1,19 @@
+//Dates
+
+let MyDate=new Date()
+console.log(MyDate);
+console.log(MyDate.getDate());
+console.log(MyDate.toDateString());
+console.log(MyDate.toISOString());
+console.log((MyDate.toLocaleString()));
+
+//let MyCreatedDate=new Date(2023,1,23) //only Date
+//console.log(MyCreatedDate.toDateString());
+
+let MyCreatedDate=new Date(2023,1,23,4,22,57) //date with time
+console.log(MyCreatedDate.toLocaleString());
+
+
+
+
+ 

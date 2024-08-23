17442db3674f2e04000cca0c8e5d096079d41eff
SaiKumarGunti08 <167128074+SaiKumarGunti08@users.noreply.github.com>
2024-08-23 16:49:45 +0000

    array

---
+++ 01_basics/08_Arrays.js
@@ -0,0 +1,35 @@
+const frstArr=[1,2,3,4,5]
+// console.log(frstArr);
+// console.log(frstArr[3]);//to find the number at this index
+// frstArr.push(6) // adds 6 to the array
+// console.log(frstArr);
+// frstArr.pop()//removes the last value
+// console.log(frstArr);
+// console.log(frstArr.indexOf(4)); // index number of number 4
+// frstArr.unshift(9) // Adds the number a the beginning
+// console.log(frstArr);
+// frstArr.shift() //removes the frst value
+// console.log(frstArr);
+// console.log(frstArr.includes(9));//to find the number included or not
+// const newArr=frstArr.join()//converts to string
+// console.log(frstArr);
+// console.log(newArr);
+
+console.log("A",frstArr);
+const myn1=frstArr.slice(1,3)
+console.log(myn1);
+
+console.log("B",frstArr);
+
+const myn2=frstArr.splice(1,3)// it manipulates the orginal array removes the data between the index
+console.log("C",frstArr);
+console.log(myn2);
+
+
+
+
+
+
+
+
+ 

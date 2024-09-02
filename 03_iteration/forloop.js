78dc77a22f830fd983102b233290d984103d1e59
SaiKumarGunti08 <167128074+SaiKumarGunti08@users.noreply.github.com>
2024-09-02 17:02:18 +0000

    for Loop

---
+++ 03_iteration/forloop.js
@@ -0,0 +1,47 @@
+// for loop
+
+// for(let i=1;i<=10;i++){
+//     const element =i;
+//     if (i==5) {
+//         console.log("5 is the middle number");
+        
+//     }
+//     console.log(element);
+// }
+// // printing tables using nested looping
+
+// for (let i =0 ; i<=10;++i) {
+//     console.log(`printing MULTIPLACTION TABLE OF : ${i}`);
+//     for (let j = 1; j <=10; j++) {
+//         console.log(`${i} x ${j} = ${i*j}`);
+                
+//     }
+    
+// }
+
+// const myFamily=["sai kumar","Aparna","chinni"]
+// for (let i = 0; i < myFamily.length; i++) {
+//     const element = myFamily[i];
+//     console.log(element);
+// }
+
+//break and continue
+//continue
+// for (let i = 0; i < 15; i++) {
+//     if (i==5 || i==10) {
+//         console.log(`value of ${i} is decteded`);
+//         continue
+//     }
+//     console.log(i);
+    
+// }
+
+//break
+for (let i = 0; i < 15; i++) {
+    if (i==5) {
+        console.log(`value of ${i} is decteded`);
+        break
+    }
+    console.log(i);
+    
+}

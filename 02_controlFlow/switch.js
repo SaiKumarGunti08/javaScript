e9001847d0df42ed8dcbd5234439ab626004e499
SaiKumarGunti08 <167128074+SaiKumarGunti08@users.noreply.github.com>
2024-09-02 12:52:12 +0000

    switch

---
+++ 02_controlFlow/switch.js
@@ -0,0 +1,33 @@
+// syntax of switch :
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month=6
+
+switch (month) {
+    case 1:
+        console.log("jan");
+        break;
+    case 2:
+        console.log("feb");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    case 4:
+        console.log("aprl");
+        break;
+    case 5:
+        console.log("may");
+        break;
+    
+    default:
+        console.log("non of the above ");
+        break;
+}

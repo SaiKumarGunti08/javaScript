d8aceffd0c4015c4a753fc0297955189d32df9ea
SaiKumarGunti08 <167128074+SaiKumarGunti08@users.noreply.github.com>
2024-09-03 08:47:54 +0000

    while loop

---
+++ 03_iteration/whileLoop.js
@@ -0,0 +1,21 @@
+//while loop
+
+let index=1
+while (index<=10) {
+    console.log(`value of index : ${index}`);
+    index++
+}
+
+const myArr=["sai","kumar","chintu"]
+let arr=0
+while (arr<myArr.length) {
+    console.log(`The ${arr}th value is ${myArr[arr]}`);
+    arr++
+}
+ //do-while Loop
+ 
+let score=11
+ do {
+    console.log(score);
+    score++
+ } while (score<=10);

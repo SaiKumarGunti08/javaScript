ccfeeae703252bfcae7fae4ff53f41ad8fa500b0
SaiKumarGunti08 <167128074+SaiKumarGunti08@users.noreply.github.com>
2024-09-05 12:14:28 +0000

    color changer

---
+++ 05_projects/01_color Change project/01_color_project.js
@@ -0,0 +1,11 @@
+const buttons=document.querySelectorAll('.button')
+const body=document.querySelector('body')
+
+buttons.forEach( (button)=>{
+    console.log(button)
+    button.addEventListener('click', (event)=>{
+        console.log(event);
+        console.log(event.target);
+        body.style.backgroundColor=event.target.id
+    })
+}  )

dd1b9328e1cfcdaaeb5dccdaa6dee564d2403b43
SaiKumarGunti08 <167128074+SaiKumarGunti08@users.noreply.github.com>
2024-09-20 13:20:44 +0000

    promise Topic

---
+++ 10_promise/01_promise.js
@@ -0,0 +1,10 @@
+/* 
+promise() :- promises are used to exicute a task or a event after a perticular time or after a condition
+
+promises are created or consumed
+
+syntax 
+create ;- const promisevar=new promise(function(resleve,reject){})
+consume :- promisevar.this(). catch()
+
+*/

28efd0bba035e439a2998b555af01d73ad8766e9
SaiKumarGunti08 <167128074+SaiKumarGunti08@users.noreply.github.com>
2024-09-20 13:46:12 +0000

    added some concepts

--- 10_promise/01_promise.js
+++ 10_promise/01_promise.js
@@ -5,6 +5,58 @@ promises are created or consumed
 
 syntax 
 create ;- const promisevar=new promise(function(resleve,reject){})
-consume :- promisevar.this(). catch()
+consume :- promisevar.this(function(){}). catch(function(){})
 
 */
+
+//m-1
+const promiseOne=new Promise(function(reslove,reject){
+    setTimeout(function(){
+        console.log('promiseOne completed');
+        reslove()
+    },1000)
+})
+promiseOne.then(function(){
+    console.log('p1 consumed');
+    
+})
+//m-2
+new Promise(function(reslove,reject){
+    setTimeout(function(){
+        console.log('promise two completed');
+        reslove()
+    },1000)
+}).then(function() {
+    console.log('p2 consumed');
+    
+})
+
+//taking data from reslove()  [ it may be object array string etc ]
+
+const promisethree=new Promise(function(reslove,reject) {
+    setTimeout(function() {
+        console.log('p3 completed');
+        reslove({username:'sai',id:1111})
+    },2000)
+})
+promisethree.then(function(user) {
+    console.log(user);
+    console.log(user.username);
+    console.log('p3 task completed');
+})
+
+//or m-2 chaining when returned (how to access the returned value) this method is used when value is returned 
+
+const promiseFour=new Promise(function(reslove,reject) {
+    setTimeout(function() {
+        console.log('p4 completed');
+        reslove({username:'chintu',id:11})
+    },2000)
+})
+promiseFour.then(function(user) {
+    console.log(user);
+    return user.username
+    
+}).then(function(username) {
+    console.log(username); 
+})
